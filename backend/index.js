43ca8c65a88e5edc34ded510f63fee4780180fef
chonlani <53692121+chonlani@users.noreply.github.com>
2020-05-05 15:13:34 +0700

    morecomment

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express(); //เป็นการสร้าง express app ขึ
 const studentRoutes = require("./routes/student");
 const userRouter = require("./routes/user")
 
-require("./config/passport/passport");
+require("./config/passport/passport"); //เป็นการบอกให้ passport สร้าง strtegy ทั้งหมดที่เราเขียนไว้
 
 
 app.use(cors());
